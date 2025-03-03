65ac642f760335aea439cb2edfd7b8e14cd971da
cesarv37 <cesaralejandrovides2023@gmail.com>
2025-03-03 01:12:22 -0400

    alert

--- src/hooks/useRegister.js
+++ src/hooks/useRegister.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { register } from '../api/authService';
+import Swal from 'sweetalert2'; // Importamos SweetAlert2
 
 const useRegister = () => {
   const [email, setEmail] = useState('');
@@ -15,6 +16,17 @@ const useRegister = () => {
     try {
       const response = await register(email, password);
       console.log('Usuario registrado:', response);
+      
+      // Alerta de éxito con SweetAlert2
+      Swal.fire({
+        icon: 'success',
+        title: '¡Registro exitoso!',
+        text: 'Ahora puedes iniciar sesión con tu cuenta.',
+      });
+
+      // Reiniciar los campos después de registro exitoso
+      setEmail('');
+      setPassword('');
     } catch (err) {
       setError(err.response?.data?.error || 'Error al registrar usuario');
     } finally {
